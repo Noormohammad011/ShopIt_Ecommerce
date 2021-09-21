4377f90519f09564efaf09ce2213f83f1d2a242f
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-09-21 13:43:18 +0600

    product details is completed

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -15,6 +18,21 @@ export const productListReducer = (state = { products: [] }, action) => {
         productsCount: action.payload.productsCount,
       }
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+export const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true,...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload
+      }
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
